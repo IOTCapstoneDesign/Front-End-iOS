d52800ddb69de1f816e6085b0877d597fa1eda79
Fiddich <78968690+Fiddich-Dev@users.noreply.github.com>
2024-03-29 21:36:19 +0900

    Add files via upload

---
+++ IOTCapstonDesign/IOTCapstonDesign/IOTCapstonDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOTCapstonDesignApp.swift
+//  IOTCapstonDesign
+//
+//  Created by 황인성 on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct IOTCapstonDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
